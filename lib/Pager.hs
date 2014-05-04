ef8a1d3b9e25a354bed793213c85380b86202266
tv <tv@nomic.retiolum>
2014-05-04 14:20:55 +0200

    Pager: externalize painting of mini windows

--- lib/Pager.hs
+++ lib/Pager.hs
@@ -2,17 +2,17 @@ module Pager
     ( pager
     , defaultPagerConfig
     , PagerConfig(..)
+    , PagerState(..)
     , PagerMatch(..)
     ) where
 
-import Control.Monad ( forM, forM_, when, zipWithM )
+import Control.Monad ( forM_, zipWithM_ )
 import Data.Char
 import Data.List
 import Data.Ord
 import Data.Map ( fromList )
 import Data.Maybe ( isJust, fromJust )
 import XMonad
-import XMonad.Actions.GridSelect ( defaultColorizer )
 import XMonad.StackSet hiding ( filter )
 import XMonad.Util.Font
 import XMonad.Util.Image ( drawIcon )
@@ -20,6 +20,8 @@ import XMonad.Util.XUtils
 
 import Debunk
 import Submap
+import Util.XUtils
+import Util.Font
 
 
 data PagerMatch = PagerMatchInfix | PagerMatchPrefix
@@ -27,23 +29,22 @@ data PagerMatch = PagerMatchInfix | PagerMatchPrefix
 data PagerConfig = PagerConfig
     { pc_font           :: String
     , pc_cellwidth      :: Dimension
-    , pc_cellheight     :: Dimension
-    --, pc_cellpadding    :: Dimension
-    , pc_borderwidth    :: Dimension
-    , pc_bordercolor    :: String
-    , pc_matchcolor     :: String
     , pc_matchmethod    :: PagerMatch
-    , pc_uncolor        :: (String, String) -- ^ color of current (unavailable) cell (background, foreground)
     , pc_wrap           :: Bool
+    , pc_colors         :: Bool -> Bool -> Bool -> (String, String, String)
+    , pc_paint          :: PagerConfig -> PagerState -> Display -> Pixmap -> GC -> String -> Rectangle -> Bool -> Bool -> Bool -> X ()
     }
 
 
-
-defaultPagerConfig = PagerConfig "xft:Sans-8" 100 30 2 "white" "magenta" PagerMatchInfix ("#232323", "#424242") True
+-- TODO currently xft is broken
+defaultPagerConfig = PagerConfig "xft:Sans-8" 100 PagerMatchInfix True stupidColors noPaint
+    where
+    stupidColors _ _ _ = ("red", "magenta", "yellow")
+    noPaint _ _ _ _ _ _ _ _ _ _ = return ()
 
 
 data PagerState = PagerState
-    { ps_windows    :: [Window]
+    { ps_window     :: Window
     , ps_search     :: String
     , ps_font       :: XMonadFont
     , ps_focus      :: (Position, Position)
@@ -63,33 +64,38 @@ match m s ws = do
         else Nothing
 
 pager :: PagerConfig -> (String -> X ()) -> [String] -> X ()
-pager c viewFunc as = newPager c as >>= ps_Mode viewFunc c
+pager pc viewFunc as = do
+    ps <- newPager pc as
+    --redraw pc ps
+    showWindow (ps_window ps)
+    pagerMode viewFunc pc ps
 
 
-ps_Mode :: (String -> X ()) -> PagerConfig -> PagerState -> X ()
-ps_Mode viewFunc c p =
+pagerMode :: (String -> X ()) -> PagerConfig -> PagerState -> X ()
+pagerMode viewFunc c p =
     case match (pc_matchmethod c) (ps_search p) (init $ ps_strings p) of
         Nothing -> redraw c p >> submapString def keys
         Just i -> removePager p >> viewFunc i
     where
     def (ch:[]) | isPrint ch =
-        incSearchPushChar ch p >>= ps_Mode viewFunc c
+        incSearchPushChar ch p >>= pagerMode viewFunc c
 
     def _ =
-        failbeep >> ps_Mode viewFunc c p
+        failbeep >> pagerMode viewFunc c p
 
     keys = fromList $
-        [ ((0, xK_BackSpace ), incSearchPopChar p >>= ps_Mode viewFunc c)
+        [ ((0, xK_BackSpace ), incSearchPopChar p >>= pagerMode viewFunc c)
         , ((0, xK_Escape    ), removePager p)
         , ((0, xK_Menu      ), removePager p)
-        , ((0, xK_Left      ), goto c (-1, 0) p >>= ps_Mode viewFunc c)
-        , ((0, xK_Right     ), goto c ( 1, 0) p >>= ps_Mode viewFunc c)
-        , ((0, xK_Up        ), goto c ( 0,-1) p >>= ps_Mode viewFunc c)
-        , ((0, xK_Down      ), goto c ( 0, 1) p >>= ps_Mode viewFunc c)
+        , ((0, xK_Left      ), goto c (-1, 0) p >>= pagerMode viewFunc c)
+        , ((0, xK_Right     ), goto c ( 1, 0) p >>= pagerMode viewFunc c)
+        , ((0, xK_Up        ), goto c ( 0,-1) p >>= pagerMode viewFunc c)
+        , ((0, xK_Down      ), goto c ( 0, 1) p >>= pagerMode viewFunc c)
         , ((0, xK_Return    ), removePager p >> return (selectFocused p) >>= viewFunc)
         ]
 
 
+-- TODO make failbeep configurable
 failbeep = spawn "beep -l 100 -f 500"
 
 
@@ -140,170 +146,154 @@ incSearchPopChar p@PagerState{ps_search=xs@(_:_)} =
 incSearchPopChar p = return p
 
 
-data TagState = Current | Candidate | Other
-    deriving (Eq)
-
-
-
 redraw :: PagerConfig -> PagerState -> X ()
-redraw c p = do
-    let w   = pc_cellwidth c
-        h   = pc_cellheight c
-        bw  = pc_borderwidth c
-        bc  = pc_bordercolor c
-        mc  = pc_matchcolor c
-
-    let wsTags = ps_strings p
-    let currentTag = last wsTags
-
-    forM_ (zip4 [1..] wsTags (ps_windows p) wave)
-          (\(i, tag, win, pos) -> do
-
-            (bg, fg) <- if pos == ps_focus p
-                            then defaultColorizer tag True
-                            else
-                                if isXOf (pc_matchmethod c) (ps_search p) tag
-                                    then colorizer c (ps_search p) currentTag tag
-                                    else return $ pc_uncolor c
-
-            let matchStuff = if tag == currentTag
-                                 then Nothing
-                                 else Just (pc_matchmethod c, ps_search p, mc)
-
-            my_paintAndWrite win (ps_font p) w h bw bg bc fg bg [AlignCenter] [tag] matchStuff)
-
-
-newPager :: PagerConfig -> [String] -> X PagerState
-newPager c as = do
-    fn <- initXMF (pc_font c)
-
+redraw pc ps = do
     ss <- gets windowset
 
     let Screen _ _ (SD (Rectangle _ _ s_width s_height)) = current ss
-        x  = fromIntegral $ s_width `div` 2
-        y  = fromIntegral $ s_height `div` 2
-        w  = pc_cellwidth c
-        h  = pc_cellheight c
-        dx = fromIntegral (pc_cellwidth c) - 1
-        dy = fromIntegral (pc_cellheight c) - 1
 
-    let wsTags = as
-    let currentTag = last wsTags
+    -- TODO this let is duplicated in newPager
+    let scale x = x * cell_w `div` s_width -- TODO use bw
+        cell_w  = pc_cellwidth pc
+        cell_h  = scale s_height
 
-    pws <- zipWithM
-            (\ tag (ox, oy) -> do
-                (bg, fg) <- colorizer c "" currentTag tag
-                createNewWindow (Rectangle (x + ox * dx) (y + oy * dy) w h) Nothing bg True)
-            wsTags
-            wave
+        -- txy is the top-left corner of the first (center) cell
+        -- XXX div and (-) are not distributive
+        --     we could round $ (s_* - cell_*) / 2, though...
+        tx = fi $ s_width  `div` 2 - cell_w `div` 2
+        ty = fi $ s_height `div` 2 - cell_h `div` 2
 
-    showWindows pws
+        -- dxy are the outer cell dimensions (i.e. including the border)
+        -- TODO currently we can only use 2 here...
+        dx = fi $ cell_w + 2
+        dy = fi $ cell_h + 2
 
-    return $ PagerState pws "" fn (0,0) wsTags
+        paint = pc_paint pc
+        xmf   = ps_font ps
+        tags  = ps_strings ps
+        --currentTag = last tags
+
+    withDisplay $ \ d -> do
+        -- XXX we cannot use withPixmapAndGC because pc_paint is an X monad
+        p <- io $ createPixmap d (ps_window ps) s_width s_height (defaultDepthOfScreen $ defaultScreenOfDisplay d)
+        g <- io $ createGC d p
+
+        -- TODO fixme
+        color_black <- stringToPixel d "black"
+
+        forZipWithM_ tags (reachableCoords ps) $ \ tag oxy@(ox, oy) -> do
+
+            let focus   = oxy == ps_focus ps
+                match   = isXOf (pc_matchmethod pc) (ps_search ps) tag
+                current = tag == last tags
+                (_b_color, _bg_color, _fg_color) = pc_colors pc focus match current
+                --cell_x = (ox * dx) + x - fi (cell_w `div` 2)
+                --cell_y = (oy * dy) + y - fi (cell_h `div` 2)
+                cell_x = (ox * dx) + tx
+                cell_y = (oy * dy) + ty
+
+            b_color <- stringToPixel d _b_color
+            bg_color <- stringToPixel d _bg_color
+            fg_color <- stringToPixel d _fg_color
+
+            -- draw background
+            io $ setForeground d g bg_color
+            io $ fillRectangle d p g (cell_x + 1) (cell_y + 1) cell_w cell_h
+
+            -- draw border
+            io $ setForeground d g b_color
+            io $ drawLines d p g
+                    [ Point cell_x cell_y
+                    , Point (fi cell_w + 1) 0
+                    , Point 0 (fi cell_h + 1)
+                    , Point (-(fi cell_w + 1)) 0
+                    , Point 0 (-(fi cell_h + 1))
+                    ]
+                    coordModePrevious
+
+            -- custom draw
+            paint pc ps d p g tag (Rectangle (cell_x + 1) (cell_y + 1) cell_w cell_h) focus match current
+
+            -- paint text
+            -- TODO custom paint text?
+            -- TODO withCopyArea
+            io $ withPixmapAndGC d p s_width s_height (defaultDepthOfScreen $ defaultScreenOfDisplay d) $ \ f_pm f_gc -> do
+                withPixmapAndGC d f_pm s_width s_height 1 $ \ clip_mask clip_gc -> do
+                    setForeground d clip_gc 0
+                    setBackground d clip_gc 0
+                    fillRectangle d clip_mask clip_gc 0 0 s_width s_height
+                    setForeground d clip_gc 1
+
+                    let r = Rectangle cell_x cell_y cell_w cell_h
+
+                    printStringCentered d clip_mask xmf clip_gc r tag
+
+                    setForeground d f_gc fg_color
+                    setBackground d f_gc color_black -- TODO
+
+                    printStringCentered d f_pm xmf f_gc r tag
+
+                    setClipMask d f_gc clip_mask
+
+                    copyArea d f_pm p f_gc 0 0 s_width s_height 0 0
+
+        io $ copyArea d p (ps_window ps) g 0 0 s_width s_height 0 0
+        io $ freePixmap d p
+        io $ freeGC d g
+
+
+newPager :: PagerConfig -> [String] -> X PagerState
+newPager c tags = do
+    ss <- gets windowset
+
+    let Screen _ _ (SD (Rectangle _ _ s_width s_height)) = current ss
+        (_, def_win_bg, _) = pc_colors c False True False
+
+    -- TODO this let is duplicated in redraw
+    let scale x = x * cell_w `div` s_width -- TODO use bw
+        cell_w  = pc_cellwidth c
+        cell_h  = scale s_height
+
+        -- TODO don't delete this let but use it instead of s_{width,height}
+        -- (xcoords, ycoords) = unzip $ take (length tags) wave -- this is reachableCoords
+        -- win_width  = (maximum xcoords - minimum xcoords) * dx
+        -- win_height = (maximum ycoords - minimum ycoords) * dy
+
+        -- txy is the top-left corner of the first (center) cell
+        -- XXX div and (-) are not distributive
+        --     we could round $ (s_* - cell_*) / 2, though...
+        tx = fi $ s_width  `div` 2 - cell_w `div` 2
+        ty = fi $ s_height `div` 2 - cell_h `div` 2
+
+        -- dxy are the outer cell dimensions (i.e. including the border)
+        -- TODO currently we can only use 2 here...
+        dx = fi $ cell_w + 2
+        dy = fi $ cell_h + 2
+
+    fn <- initXMF (pc_font c)
+    win <- createNewWindow (Rectangle 0 0 s_width s_height) Nothing def_win_bg True
+
+    withDisplay $ \ d ->
+        io $ shapeWindow d win $ \ p g ->
+            forZipWithM_ tags wave $ \ _ (ox, oy) ->
+                fillRectangle d p g (tx + ox * dx) (ty + oy * dy) (fi dx) (fi dy)
+
+    return $ PagerState win "" fn (0,0) tags
 
 
 removePager :: PagerState -> X ()
-removePager (PagerState pws _ fn _ _) = do
+removePager (PagerState w _ fn _ _) = do
+    deleteWindow w
     releaseXMF fn
-    deleteWindows pws
-
-
-tagState searchInput currentTag t
-    | t == currentTag = Current
-    | length searchInput > 0 && searchInput `isPrefixOf` t = Candidate
-    | otherwise = Other
-
-
-colorizer :: PagerConfig -> String -> String -> String -> X (String, String)
-colorizer c searchInput currentTag tag =
-    case tagState searchInput currentTag tag of
-        Current     -> return $ pc_uncolor c
-        --Candidate   -> defaultColorizer tag True
-        _           -> defaultColorizer tag False
-
-
-
-
 
 wave :: [(Position, Position)]
 wave = zip (0:(concat $ map (\i -> [0..i]++[i-1,i-2..1] ++ [0,-1..(-i)]++[-i,-i+1..(-1)]) [1..])) (concat $ map (\i -> [0..i]++[i-1,i-2..1] ++ [0,-1..(-i)]++[-i+1,-i+2..(-1)]) [1..])
     where
         wave1 = 0:(concat $ map (\i -> [0..i]++[i-1,i-2..1] ++ [0,-1..(-i)]++[-i,-i+1..(-1)]) [1..])
         wave2 = concat $ map (\i -> [0..i]++[i-1,i-2..1] ++ [0,-1..(-i)]++[-i+1,-i+2..(-1)]) [1..]
-
 
--- | Fill a window with a rectangle and a border, and write
--- | a number of strings to given positions
-my_paintAndWrite :: Window     -- ^ The window where to draw
-                -> XMonadFont -- ^ XMonad Font for drawing
-                -> Dimension  -- ^ Window width
-                -> Dimension  -- ^ Window height
-                -> Dimension  -- ^ Border width
-                -> String     -- ^ Window background color
-                -> String     -- ^ Border color
-                -> String     -- ^ String color
-                -> String     -- ^ String background color
-                -> [Align]    -- ^ String 'Align'ments
-                -> [String]   -- ^ Strings to be printed
-                -> Maybe (PagerMatch, String, String) -- ^ TODO
-                -> X ()
-my_paintAndWrite w fs wh ht bw bc borc ffc fbc als strs matchStuff = do
-    d <- asks display
-    strPositions <- forM (zip als strs) $ \(al, str) ->
-        stringPosition d fs (Rectangle 0 0 wh ht) al str
-    let ms = Just (fs,ffc,fbc, zip strs strPositions)
-    my_paintWindow' w (Rectangle 0 0 wh ht) bw bc borc ms Nothing matchStuff
-
--- | Paints a titlebar with some strings and icons
--- drawn inside it.
--- Not exported.
-my_paintWindow' :: Window -> Rectangle -> Dimension -> String -> String
-                -> Maybe (XMonadFont,String,String,[(String, (Position, Position))])
-                -> Maybe (String, String, [((Position, Position), [[Bool]])])
-                -> Maybe (PagerMatch, String, String) -- ^ TODO
-                -> X ()
-my_paintWindow' win (Rectangle _ _ wh ht) bw color b_color strStuff iconStuff matchStuff = do
-  d  <- asks display
-  p  <- io $ createPixmap d win wh ht (defaultDepthOfScreen $ defaultScreenOfDisplay d)
-  gc <- io $ createGC d p
-  -- draw
-  io $ setGraphicsExposures d gc False
-  [color',b_color'] <- mapM (stringToPixel d) [color,b_color]
-  -- we start with the border
-  io $ setForeground d gc b_color'
-  io $ fillRectangle d p gc 0 0 wh ht
-  -- and now again
-  io $ setForeground d gc color'
-  io $ fillRectangle d p gc (fi bw) (fi bw) (wh - (bw * 2)) (ht - (bw * 2))
-  -- paint strings
-  when (isJust strStuff) $ do
-    let (xmf,fc,bc,strAndPos) = fromJust strStuff
-    forM_ strAndPos $ \(s, (x, y)) -> do
-        printStringXMF d p xmf gc fc bc x y s
-
-        when (isJust matchStuff) $ do
-            let Just (matchMethod, searchInput, matchColor) = matchStuff
-                mbi = findXIndex matchMethod searchInput s
-            when (isJust mbi) $ do
-                let i = fromJust mbi
-                skip_width <- textWidthXMF d xmf (take i s)
-
-                printStringXMF d p xmf gc matchColor bc (x + fromIntegral skip_width) y searchInput
-
-  -- paint icons
-  when (isJust iconStuff) $ do
-    let (fc, bc, iconAndPos) = fromJust iconStuff
-    forM_ iconAndPos $ \((x, y), icon) ->
-      drawIcon d p gc fc bc x y icon
-  -- copy the pixmap over the window
-  io $ copyArea      d p win gc 0 0 wh ht 0 0
-  -- free the pixmap and GC
-  io $ freePixmap    d p
-  io $ freeGC        d gc
-
-
 commonPrefix (x:xs) (y:ys) | x == y = x:commonPrefix xs ys
-commonPrefix _ _ = [] 
+commonPrefix _ _ = []
 
 
 isXOf :: PagerMatch -> String -> String -> Bool
@@ -322,6 +312,7 @@ findInfixIndex needle haystack
       . dropWhile (\(_,x) -> not $ isPrefixOf needle x)
         $ zip [0..] (tails haystack)
 
+
 findPrefixIndex :: (Eq a) => [a] -> [a] -> Maybe Int
 findPrefixIndex needle haystack =
     if isPrefixOf needle haystack
@@ -331,3 +322,6 @@ findPrefixIndex needle haystack =
 
 modIndex :: Integral i => [a] -> i -> a
 modIndex xs i = xs `genericIndex` (i `mod` genericLength xs)
+
+
+forZipWithM_ a b f = zipWithM_ f a b
